cec3bed846cd4a71185e61d6f92d1a768c89d346
Milix-M <tujikou12dh@gmail.com>
2023-10-06 18:06:06 +0900

    first commit

---
+++ front/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Nuxt3 Jamstack Blogs</h1>
+  <ul>
+    <li v-for="blog in data?.contents" :key="blog.id">
+      <NuxtLink :to="`/${blog.id}`">
+        <img
+          :src="blog.eyecatch?.url"
+          :width="blog.eyecatch?.width"
+          :height="blog.eyecatch?.height"
+          alt=""
+        />
+        <div>
+          <div>
+            {{ blog.category?.name }}
+          </div>
+          <div>
+            {{ blog.title }}
+          </div>
+          <div>
+            {{ dateFormat(data.publishedAt ?? data.createdAt) }}
+          </div>
+        </div>
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { Blog } from "~~/types/Blog";
+
+const { data } = await useMicroCMSGetList<Blog>({
+  endpoint: "blogs",
+});
+</script>
